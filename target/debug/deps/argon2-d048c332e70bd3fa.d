68b848ddc7ce6658d691fef6eb4721e24e748d7e
pas2rust <145881352+pas2rust@users.noreply.github.com>
2024-01-21 10:06:25 -0300

    up

--- target/debug/deps/argon2-d048c332e70bd3fa.d
+++
@@ -1,5 +0,0 @@
-/home/pas/Desktop/git/wrapper-argon2/target/debug/deps/libargon2-d048c332e70bd3fa.rmeta: tests/argon2.rs
-
-/home/pas/Desktop/git/wrapper-argon2/target/debug/deps/argon2-d048c332e70bd3fa.d: tests/argon2.rs
-
-tests/argon2.rs:
--- target/debug/deps/wrapper_argon2-fe02bb0fd1f55809.d
+++
@@ -1,6 +0,0 @@
-/home/pas/Desktop/git/wrapper-argon2/target/debug/deps/libwrapper_argon2-fe02bb0fd1f55809.rmeta: src/lib.rs src/wrapper.rs
-
-/home/pas/Desktop/git/wrapper-argon2/target/debug/deps/wrapper_argon2-fe02bb0fd1f55809.d: src/lib.rs src/wrapper.rs
-
-src/lib.rs:
-src/wrapper.rs:
